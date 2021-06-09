fd1253b4c9f05c6c661caa27ef010179cd049ed0
schoolerBurtz <85542135+schoolerBurtz@users.noreply.github.com>
2021-06-09 10:55:37 +0200

    Update Data.java

--- src/main/java/me/fingolfin/Data.java
+++ src/main/java/me/fingolfin/Data.java
@@ -27,8 +27,6 @@ public class Data implements AutoCloseable {
   
   public ResultSet getResults(String query) {
         try {
-            con = DriverManager.getConnection(jdbcUrl);
-            stmnt = con.createStatement();
             ResultSet set = stmnt.executeQuery(query);
             return set;
 
@@ -36,6 +34,44 @@ public class Data implements AutoCloseable {
             throwables.printStackTrace();
             System.out.println("query gone wrong");
             return null;
+        }
+    }
+  
+   public static void insert(String table, String[] rows, String[] values) {
+        try {
+            String query = "insert into " + table + " (";
+            for (String s : rows) {
+                 query = query + "\""+  s +"\",";
+            }
+            query = query.substring(0, query.length() -1);
+            query = query + ") values (";
+            for (String s : values) {
+                query = query + "\""+  s +"\",";
+            }
+            query = query.substring(0, query.length() -1);
+            query = query + ");";
+
+            stmnt.execute(query);
+
+
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+            System.out.println("query gone wrong");
+        }
+    }
+  
+   public static void updateVal (String table, String row, String val, String condition_key, String condition_val) {
+        try {
+            String query = String.format("update %s set %s = \"%s\" where %s = \"%s\";", table, row, val,
+                    condition_key, condition_val);
+            System.out.println(query);
+            stmnt.execute(query);
+
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+            System.out.println("query gone wrong");
         }
     }
   
