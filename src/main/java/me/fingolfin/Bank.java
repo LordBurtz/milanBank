18feec36881bba31a99dec581c7a7b40fd9838aa
schoolerBurtz <85542135+schoolerBurtz@users.noreply.github.com>
2021-06-09 11:13:03 +0200

    Update Bank.java

--- src/main/java/me/fingolfin/Bank.java
+++ src/main/java/me/fingolfin/Bank.java
@@ -84,14 +84,14 @@ public class Bank {
         ResultSet set = data.getResults("select worth from customers where id  = \"" + senderID + "\"");
         int amount_old = set.getInt("worth");
         int new_amount = amount_old - amount;
-        updateCustomer(senderID, "worth", new_amount);
+        updateCustomer(senderID, "worth", new_amount.toString());
         
         System.out.println("sender updated");
         
         set = data.getResults("select worth from customers where id  = \"" + recieverID + "\"");
         amount_old = set.getInt("worth");
         new_amount = amount_old + amount;
-        updateCustomer(recieverID, "worth", new_amount);
+        updateCustomer(recieverID, "worth", new_amount.toString());
         
         System.out.println("receiver updated");
     }
